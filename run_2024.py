24c23d36b452bd922090118b70e49b9a2aa78f5b
nichco <norndorff@live.com>
2024-01-29 16:11:32 -0800

    more run script work

--- run_2024.py
+++ run_2024.py
@@ -4,13 +4,19 @@ import m3l
 import numpy as np
 from python_csdl_backend import Simulator
 import lsdo_geo as lg
+from lsdo_rotor import BEMParameters, evaluate_multiple_BEM_models, BEM
+from VAST import FluidProblem, VASTFluidSover, VASTNodalForces
+import caddee.api as cd
 from caddee.utils.helper_functions.geometry_helpers import (make_rotor_mesh, make_vlm_camber_mesh, make_1d_box_beam_mesh, 
                                                             compute_component_surface_area, BladeParameters)
 
+
+caddee = cd.CADDEE()
 geometry = lg.import_geometry('darpa4.stp', parallelize=True)
 geometry.refit(parallelize=True, fit_resolution=(50, 50), num_coefficients=(30, 30),)
 # geometry.plot()
-m3l_model = m3l.Model()
+# m3l_model = m3l.Model()
+system_model = m3l.Model()
 
 wing = geometry.declare_component(component_name='wing', b_spline_search_names=['Wing'])
 htail = geometry.declare_component(component_name='htail', b_spline_search_names=['OldTail'])
@@ -70,7 +76,7 @@ tail_meshes = make_vlm_camber_mesh(
 
 
 
-# # wing beam mesh
+# # wing beam mesh (old way)
 # eps = 2
 # num_wing_beam = 15
 # leading_edge = wing.project(np.linspace(np.array([14.803-eps, -29.986, 1.455]), np.array([14.803-eps, 29.986, 1.455]), num_wing_beam), direction=np.array([0., 0., -1.]), plot=True)
@@ -80,7 +86,7 @@ tail_meshes = make_vlm_camber_mesh(
 # width = m3l.norm((geometry.evaluate(leading_edge) - geometry.evaluate(trailing_edge))*0.5)
 # # geometry.plot_meshes([wing_beam.reshape((-1,3))])
 
-
+# wing beam mesh (helper)
 wing_beam_mesh = make_1d_box_beam_mesh(
     geometry=geometry,
     wing_component=wing,
@@ -126,3 +132,62 @@ rfuse_front = rfuse.project(np.array([0, 7, 0.5]), plot=False)
 rfuse_back = rfuse.project(np.array([27, 7, 0.81]), plot=False)
 rfuse_beam_mesh = m3l.linspace(geometry.evaluate(rfuse_front), geometry.evaluate(rfuse_back), num_fuse_beam)
 # geometry.plot_meshes([rfuse_beam_mesh.reshape((-1,3))])
+
+
+
+
+
+
+
+cruise = True
+plus_3g = True
+
+if cruise:
+    cruise_condition = cd.CruiseCondition(
+        name='steady_cruise',
+        num_nodes=1,
+        stability_flag=True,
+    )
+
+    cruise_M = system_model.create_input('cruise_mach', val=0.195)
+    cruise_h = system_model.create_input('cruise_altitude', val=1000)
+    cruise_range = system_model.create_input('cruise_range', val=60000)
+    cruise_pitch = system_model.create_input('cruise_pitch', val=np.deg2rad(0), dv_flag=True, lower=np.deg2rad(-5), upper=np.deg2rad(5), scaler=10)
+
+    cruise_ac_states, cruise_atmos = cruise_condition.evaluate(mach_number=cruise_M, pitch_angle=cruise_pitch, altitude=cruise_h, cruise_range=cruise_range)
+
+    system_model.register_output(cruise_ac_states)
+    system_model.register_output(cruise_atmos)
+
+    # cruise_bem = BEM(
+    #     name='cruise_bem',
+    #     num_nodes=1, 
+    #     BEM_parameters=bem_pusher_rotor_parameters,
+    #     rotation_direction='ignore',
+    # )
+    # cruise_rpm = system_model.create_input('cruise_rpm', val=2000, dv_flag=True, lower=600, upper=2500, scaler=1e-3)
+    # cruise_bem_outputs = cruise_bem.evaluate(ac_states=cruise_ac_states, rpm=cruise_rpm, rotor_radius=pp_mesh.radius, thrust_vector=pp_mesh.thrust_vector,
+    #                                                 thrust_origin=pp_mesh.thrust_origin, atmosphere=cruise_atmos, blade_chord_cp=pp_mesh.chord_cps, blade_twist_cp=pp_mesh.twist_cps, 
+    #                                                 cg_vec=m4_mass_properties.cg_vector, reference_point=m4_mass_properties.cg_vector)
+    # system_model.register_output(cruise_bem_outputs)
+
+
+
+    # vlm_model = VASTFluidSover(
+    #     name='cruise_vlm_model',
+    #     surface_names=[
+    #         'cruise_wing_mesh',
+    #         'cruise_tail_mesh',
+    #         'cruise_vtail_mesh',
+    #         'cruise_fuselage_mesh',
+    #     ],
+    #     surface_shapes=[
+    #         (1, ) + wing_meshes.vlm_mesh.shape[1:],
+    #         (1, ) + tail_meshes.vlm_mesh.shape[1:],
+    #         (1, ) + vtail_meshes.vlm_mesh.shape[1:],
+    #         (1, ) + fuesleage_mesh.shape,
+    #     ],
+    #     fluid_problem=FluidProblem(solver_option='VLM', problem_type='fixed_wake', symmetry=True),
+    #     mesh_unit='ft',
+    #     cl0=[0., 0., 0., 0.]
+    # )
